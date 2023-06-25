b715ef5a53859b809fb4fa845c643583f8af29f8
UttejK <uttej09092001@gmail.com>
2023-06-25 21:30:45 +0530

    cleanup v1

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,20 @@ function App() {
   const hf = new HfInference(import.meta.env.VITE_HF_API_KEY);
   const [prompt, setPrompt] = useState("Golden Temple of India");
 
-  const [negPrompt, setNegPrompt] = useState("blurry");
+  const [negPrompt, setNegPrompt] = useState(
+    "worst quality, low quality, anime, cartoon, mutilated, out of frame, extra fingers, mutated hands, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, bad face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, deformed tongue, children preteen kids jpeg artifacts, text, dark skin, nsfw, nude, naked"
+  );
 
   const [loading, setLoading] = useState(false);
 
   const [model, setModel] = useState("prompthero/openjourney-v4");
 
   const [url, setUrl] = useState(
-    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/02/3c/e2/2c/golden-temple-3.jpg?w=1200&h=-1&s=1"
+    "https://images.pexels.com/photos/6152103/pexels-photo-6152103.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   );
+
+  const [height, setHeight] = useState(512);
+  const [width, setWidth] = useState(512);
 
   const size = 512;
 
@@ -45,7 +50,7 @@ function App() {
         try {
           data = res.data["secure_url"];
         } catch {
-          console.log(err);
+          // console.log(err);
         }
       });
     // console.log(data);
@@ -61,8 +66,8 @@ function App() {
           model: model,
           inputs: prompt,
           parameters: {
-            width: size,
-            height: size,
+            width: width,
+            height: height,
             negative_prompt: negPrompt,
           },
         })
@@ -70,55 +75,33 @@ function App() {
           const image = new Image();
           // image.src = URL.createObjectURL(blob);
           const cloudinaryURL = await uploadToCloudinary(blob);
-          console.log(cloudinaryURL);
+          // console.log(cloudinaryURL);
           return cloudinaryURL;
         });
-      // console.log(model);
       setUrl(response);
-      // console.log(response);
-      // document.getElementsByClassName("promptInput")[0].value = "";
       setLoading(false);
     }
     console.log("render ended");
   };
 
   return (
-    <main
-      style={{
-        height: "100vh",
-        width: "100vw",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <div
-        className="card"
-        style={{
-          width: "32rem",
-        }}
-      >
+    <main className="w-[100vw] h-[100vh] flex flex-col px-12 items-center justify-center">
+      <div className="h-3/4 flex flex-col items-center text-center">
+        <h1 className="text-3xl bg-gray-300 w-full rounded-t-md py-1">
+          AI Image Generator
+        </h1>
         {loading ? (
-          <>
-            <div
-              style={{
-                width: "100%",
-                minHeight: "20vh",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <div className="spinner-border" role="status">
-                <span className="visually-hidden">Loading...</span>
-              </div>
-            </div>
-          </>
+          <span style={{ width: width, height: height }}>Loading...</span>
         ) : (
-          <img src={url} alt="The Rendered Image" className="card-img-top" />
+          <img
+            src={url}
+            alt="The Rendered Image"
+            className="object-contain"
+            style={{ width: width, height: height }}
+          />
         )}
         <select
-          className="form-select"
+          className="form-select w-full"
           onChange={(e) => {
             // console.log(e.target.value);
             setModel(e.target.value);
@@ -132,25 +115,63 @@ function App() {
           </option>
         </select>
         <input
-          className="promptInput"
+          className="promptInput w-full form-input"
           type="text"
           placeholder="Please enter the Prompt"
           onChange={(e) => {
-            console.log("prompt:", e.target.value);
+            // console.log("prompt:", e.target.value);
             setPrompt(e.target.value);
           }}
         />
         <input
-          className="promptInput"
+          className="promptInput w-full form-input"
           type="text"
           placeholder="Please enter the Negative prompt"
           onChange={(e) => {
-            console.log("negPrompt:", e.target.value);
+            // console.log("negPrompt:", e.target.value);
             setNegPrompt(e.target.value);
           }}
         />
+
+        <div className="w-full justify-between flex gap-1">
+          <label htmlFor="height" className="w-max">
+            Height
+          </label>
+          <input
+            type="range"
+            className="w-3/5 inline"
+            min="256"
+            max="1024"
+            step="8"
+            onChange={(e) => {
+              // console.log(e.target.value);
+              setHeight(parseInt(e.target.value));
+            }}
+            defaultValue={height}
+          ></input>
+          <label htmlFor="heightVal">{height}</label>
+        </div>
+
+        <div className="w-full justify-between flex gap-1">
+          <label htmlFor="width" className="w-max">
+            Width
+          </label>
+          <input
+            type="range"
+            className="w-3/5 inline"
+            min="256"
+            max="1024"
+            step="8"
+            onChange={(e) => {
+              // console.log(e.target.value);
+              setWidth(parseInt(e.target.value));
+            }}
+            defaultValue={height}
+          ></input>
+          <label htmlFor="widthVal">{width}</label>
+        </div>
         <button
-          className="btn btn-primary"
+          className="w-full bg-blue-400 rounded-lg py-3 hover:bg-blue-700"
           type="submit"
           onClick={() => {
             setLoading(true);
